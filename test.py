701af97032d2db4b453ad6847cbc99b0ca6377b8
Enes Demirag <ensdmrg@gmail.com>
2020-04-21 23:56:25 +0300

    Generate map testing script

---
+++ test.py
@@ -0,0 +1,19 @@
+# Import dependencies
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+from random import randint
+import matplotlib.pyplot as plt
+
+# Load model
+g = tf.keras.models.load_model('mapGAN.h5', compile=False)
+
+# Generate
+noise = tf.random.normal([1, randint(0, 1000)])
+generated_map = g(noise, training=False)
+
+# Show generated image
+plt.imshow(generated_map[0, :, :, 0])
+plt.show()
+im = Image.fromarray(generated_map)
+im.save("map.png")
