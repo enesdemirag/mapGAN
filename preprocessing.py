7c9e13c6d29100ce8dbf233411c4d2a25bce8d49
Enes Demirag <ensdmrg@gmail.com>
2020-04-21 20:36:11 +0300

    Fix tensor shape (3, 64, 64) to (64, 64, 3)

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from tqdm import tqdm
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
 # Get images names
@@ -11,15 +12,15 @@ resolution = 64
 channels = 3
 
 # Create array
-dataset = np.ndarray(shape=(len(images), channels, resolution, resolution), dtype=np.float32)
+dataset = np.ndarray(shape=(len(images), resolution, resolution, channels), dtype=np.float32)
 
 i = 0
-for image in images:
+for image in tqdm(images):
     img = load_img(dataset_path + "/" + image)              # PIL image
     img.thumbnail((resolution, resolution))                 # Resize
 
     x = img_to_array(img)                                   # Convert to numpy array
-    x = x.reshape((channels, resolution, resolution))       # Reshape
+    x = x.reshape((resolution, resolution, channels))       # Reshape
     x = (x - 128.0) / 128.0                                 # Normalize (Between -1 to +1)
     dataset[i] = x
     i = i + 1
