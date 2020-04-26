1300030608943c80c5dddc51d278780d6f340545
enesdemirag <ensdmrg@gmail.com>
2020-04-26 18:40:33 +0300

    Failed load model

--- test.py
+++ test.py
@@ -7,9 +7,10 @@ import matplotlib.pyplot as plt
 
 # Load model
 g = tf.keras.models.load_model('mapGAN.h5', compile=False)
+g.compile(loss='binary_crossentropy', optimizer='adam')
 
 # Generate
-noise = tf.random.normal([1, randint(0, 1000)])
+noise = tf.random.normal([1, randint(0, 100)])
 generated_map = g(noise, training=False)
 
 # Show generated image
